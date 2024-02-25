1364748ecdeb84d09fc8392c78923ed0dae9dd71
Lymix <137233683+Lymiix@users.noreply.github.com>
2024-02-25 17:35:53 +0200

    Delete 03 directory

--- 03/src/mx_printerror.c
+++
@@ -1,5 +0,0 @@
-#include "../inc/matrix_rain.h"
-
-void mx_printerror(char *str) {
-    write(STDERR_FILENO, str, mx_strlen(str));
-}
--- 03/src/screensaver_destroy.c
+++
@@ -1,7 +0,0 @@
-#include "../inc/matrix_rain.h"
-
-void screensaver_destroy(WINDOW *window) {
-    delwin(window);
-    endwin();
-    exit(0);
-}
